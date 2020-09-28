c04f1760a5c911f1cc546abb495b27181e59acaf
hephka <franckthevenin95@gmail.com>
2020-09-28 18:03:13 +0200

    optionnel ok

--- src/promiseCopy.js
+++ src/promiseCopy.js
@@ -4,6 +4,22 @@ import { COPYFILE_EXCL } from 'constants'
 /* Ecrivez un programme qui copie le fichier file1.txt vers le fichier file2.txt
  en utilisant les api asynchrones basées sur des promises du module fs de node.js. */
 
- fsPromises.copyFile('file1.txt', 'file2.txt', COPYFILE_EXCL)
+ /*     Réecrivez le programme promiseCopy.js afin qu'il puisse accepter 2 arguments de la ligne de commande:
+
+un fichier source, qui est le fichier à copier
+un fichier dest, qui sera la destination de la copie
+Comme pour l'exercice précedent utiliser les api asynchrones basées sur des promises du module fs de node.js. */
+
+let source = process.argv[2]
+let dest = process.argv[3]
+
+ fsPromises.copyFile(source, dest, COPYFILE_EXCL)
     .then(() => console.log('source.txt was copied to destination.txt'))
     .catch(() => console.log('The file could not be copied'))
+
+if (!isNaN(source) || !isNaN(dest)) {
+    console.log('Error: source<String> ; dest<String>')
+}
+if (process.argv.length !== 4) {
+    console.log('Usage : node promiseCopy.js source dest')
+}
